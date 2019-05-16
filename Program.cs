19b10689b1b4059151f903bacea2b4296db8637c
Danyer Dominguez <danjer0826@gmail.com>
2019-05-16 12:44:32 -0400

    Added database connection.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,13 +10,6 @@ namespace TextFileGeneration
 {
     class Program
     {
-        public static FileGenerationContext _context;
-
-        public Program(FileGenerationContext context)
-        {
-            _context = context;
-        }
-
         static void Main(string[] args)
         {
             Console.Write("Escribe la ruta final donde quiere que se imprima el archivo: ");
@@ -31,36 +25,6 @@ namespace TextFileGeneration
                 File.Delete(path);    
             }
 
-            Institution institution = new Institution()
-            {
-                AccountNumber = 1,
-                PaymentDate = DateTime.Now,
-                RNC = "345457234",
-                TransmitionDate = DateTime.Now,
-                EmployeesNumber = 19,
-                TotalAmount = 113213234.123,
-                Employees = new HashSet<Employee>
-                {
-                    new Employee
-                    {
-                        AccountNumber = "00145511",
-                        InstitutionId = 1,
-                        EmployeeCode = "EM009",
-                        Salary = 11478.58,
-                        Id = "00179846214"
-                    },
-                    new Employee
-                    {
-                        AccountNumber = "00145513",
-                        InstitutionId = 1,
-                        EmployeeCode = "EM010",
-                        Salary = 11478.58,
-                        Id = "00179845544"
-                    }
-                }
-            };
-
-
             if (!Directory.Exists(path))
             {
                 Directory.CreateDirectory(path);
@@ -68,16 +32,22 @@ namespace TextFileGeneration
             
             using (StreamWriter file = File.CreateText($@"{path}\archivo.txt"))
             {
-                file.WriteLine("#Encabezado");
-                file.WriteLine($"{institution.AccountNumber},{institution.PaymentDate},{institution.RNC}," +
-                    $"{institution.TransmitionDate},{institution.TotalAmount}");
-                file.WriteLine("#Detalle");
-                foreach (Employee employee in institution.Employees)
+                using (var context = new FileGenerationContext())
                 {
-                    file.WriteLine($"{employee.AccountNumber},{employee.Salary},{employee.InstitutionId},{employee.EmployeeCode}");
+                    Institution foudInstitution = context.Institutions.Include(institution => institution.Employees)
+                        .FirstOrDefault(institution => institution.Id == 1);
+
+                    file.WriteLine("#Encabezado");
+                    file.WriteLine($"{foudInstitution.AccountNumber},{foudInstitution.PaymentDate},{foudInstitution.TransmitionDate}," +
+                        $"{foudInstitution.RNC},{foudInstitution.TotalAmount}");
+                    file.WriteLine("#Detalle");
+                    foreach (Employee employee in foudInstitution.Employees)
+                    {
+                        file.WriteLine($"{employee.Id},{employee.AccountNumber},{employee.Salary},{employee.EmployeeCode}");
+                    }
+                    file.WriteLine("#Sumario");
+                    file.WriteLine($"{foudInstitution.Employees.Count()}");
                 }
-                file.WriteLine("#Sumario");
-                file.WriteLine($"{institution.EmployeesNumber}");
             }
         }
     }
