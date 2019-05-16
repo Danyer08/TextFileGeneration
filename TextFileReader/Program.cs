3f65d81be18a89d4c93abb8f71fd058045ee4f7f
luisjjavier <javier.luis29@gmail.com>
2019-05-16 13:14:59 -0400

    Addes changes for work

--- TextFileReader/Program.cs
+++ TextFileReader/Program.cs
@@ -9,13 +9,13 @@ namespace TextFileReader
     {
         static void Main(string[] args)
         {
-            Console.Write("Seleccione la de donde se procederan a leer los archivos");
+            Console.Write("Seleccione la de donde se procederan a leer los archivos: ");
             string route = Console.ReadLine();
 
             using (var fs = new FileStream(route, FileMode.Open, FileAccess.Read))
             using (StreamReader reader = new StreamReader(fs))
             {
-                Transformator tf = new Transformator();
+                var tf = new Transformator();
 
                 Archivo archivo =  tf.Transform(reader);
                 archivo.FileName = fs.Name;
@@ -23,11 +23,32 @@ namespace TextFileReader
 
                 context.Archivos.Add(archivo);
                 context.SaveChanges();
+
+                PrintInvoice(archivo);
             }
 
-            Console.WriteLine($"Archivo leido correctamente");
             Console.ReadLine();
         }
 
+        private static void PrintInvoice(Archivo archivo)
+        {
+            Console.WriteLine("".PadRight(40, '='));
+            Console.WriteLine($"RNC: {archivo.RNC}");
+            Console.WriteLine($"Monto a pagar: {archivo.Roster:C0}");
+            Console.WriteLine($"Fecha de pago: {archivo.PaymentDate:F}");
+            Console.WriteLine("");
+            Console.WriteLine("Detalle:");
+
+            Console.WriteLine("".PadRight(40, '='));
+            Console.WriteLine($"No. Cuenta\tMonto a pagar ");
+            Console.WriteLine("".PadRight(40, '='));
+            foreach (var detail in archivo.FileDetail)
+            {
+                Console.WriteLine($"{detail.AccountNumber}\t{detail.Salary:C0}");
+            }
+
+
+            Console.WriteLine("".PadRight(40, '='));
+        }
     }
 }
